180b43e901c1760ef20219d9e4e0c7eb89f105cf
JuanMMorante <moranteconsuegrajuanmanuel@gmail.com>
2025-09-27 13:12:14 -0500

    Delete run_etl.sh

--- run_etl.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Ejecutar ETL DW (Linux/macOS)
-export PGHOST=localhost
-export PGPORT=5432
-export PGUSER=postgres
-export PGPASSWORD=TU_PASS
-export PGDATABASE=dw_salud
-
-python3 etl_dw_postgres.py
